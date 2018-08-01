0e06e297276b97a89fca817412f87ef28d61a79c
Jürgen Weigert <jw@owncloud.com>
2018-08-01 23:23:01 +0200

    ocr test updated.

--- test/ocr.py
+++ test/ocr.py
@@ -1,5 +1,19 @@
 #! /usr/bin/python3
 #
+# Ubuntu-18.04 has a stone-age old pyocr and stone age old pip.
+# We don't want to reproduce old bugs, we want something new. 
+# Better bootstrap from pipy than from the ubuntu repo:
+# Ubuntu repo:
+# ( sudo apt install python3-pyocr		# 0.3.0		hmm. )
+#
+# Pypi:
+# sudo apt install python3-pip tesseract-ocr-eng tesseract-ocr-deu
+# sudo -H pip3 install --upgrade pip setuptools
+# sudo -H pip3 install --upgrade pyocr		# 0.5.2		yeah,
+#
+# manual ocr:
+# ocrmypdf -l eng+deu --image-dpi 70 --oversample 140  ~/Bilder/Bildschirmfoto\ vom\ 2018-07-31\ 15-25-35.png out.pdf; less out.pdf > 140dpi.txt
+
 
 from PIL import Image
 import sys
@@ -14,15 +28,28 @@ img=Image.open(infile)
 textord_min_xheight = 10 # from the docs
 
 tools = pyocr.get_available_tools()
+if len(tools) == 0:
+  print("ERROR: Tesseract not found.\nTry:\n\tsudo apt install tesseract-ocr-deu tesseract-ocr-eng")
+  sys.exit(1)
+  
 # The tools are returned in the recommended order of usage
-tool = tools[1] ## prefer libtesseract over tesseract shell over cuneyform
+# but we prefer libtesseract over tesseract shell.
+if len(tools) > 1 and tools[1].get_name() == 'Tesseract (C-API)':
+  tool = tools[1] ## prefer libtesseract over tesseract shell over cuneyform
+else:
+  tool = tools[0]
 
 langs = tool.get_available_languages()
 # Note that languages are NOT sorted in any way. Please refer
 # to the system locale settings for the default language
 # to use.
-print(tools)
-print(langs)
+print("Using tool: ", tool.get_name())
+for need_lang in ('deu', 'eng'):
+  if need_lang not in langs:
+    print("ERROR: language '"+need_lang+"' is not available with tool "+str(tool))
+    sys.exit(1)
+
+# print(langs)
 
 def avg_dev(data, dev=-1):
   """calculate the value of dev standard-deviations away
@@ -49,7 +76,8 @@ def word_stats(wordboxdata):
 
 
 lang='eng'
-# lang='deu'
+lang='deu'
+lang='deu+eng'	# multi language!
 words = tool.image_to_string(img, lang=lang, builder=pyocr.builders.WordBoxBuilder() )
 
 # tesseract has textord_min_xheight=10  # Min credible pixel xheight
@@ -74,9 +102,13 @@ else:
 if scale > 1:
   print("avg1d(height)=%g, avg1d(confidence)=%g -> scaling up by %g" % (a1d_h, a1d_c, scale))
   img=img.resize( (int(0.5+img.width*scale), int(0.5+img.height*scale)), Image.BICUBIC)
-  words = tool.image_to_string(img, lang=lang, builder=pyocr.builders.WordBoxBuilder() )
-  (a1d_c, a1d_h, tlen) = word_stats(words)
-  print("improved avg1d(confidence)=%g" % (a1d_c))
+  s_words = tool.image_to_string(img, lang=lang, builder=pyocr.builders.WordBoxBuilder() )
+  (s_a1d_c, s_a1d_h, s_tlen) = word_stats(s_words)
+  if s_a1d_c > a1d_c:
+    print("improved avg1d(confidence)=%g" % (a1d_c))
+    (a1d_c, a1d_h, tlen, words) = (s_a1d_c, s_a1d_h, s_tlen, s_words)
+  else:
+    print("wasted scale-up: avg1d(confidence)=%g decreased from %g" % (s_a1d_c, a1d_c))
 
 
 print(infile)
